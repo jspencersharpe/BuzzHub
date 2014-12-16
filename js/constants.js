82e189c1e935deaf8d726e6adba516e6b64cacb6
Spencer Sharpe <jspencersharpe@gmail.com>
2014-12-16 16:00:46 -0600

    began authentication

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,7 @@
 ;(function(){
   'use strict';
+  
+  angular.module('buzzHub')
+    .constant('FIREBASE_URL', 'https://buzzhub.firebaseio.com/')
+
 }());
--- js/main.js
+++ js/main.js
@@ -2,6 +2,48 @@
   'use strict';
 
   angular.module('buzzHub', [])
+    .factory('authFactory', function($routeScope, $location, FIREBASE_URL){
+      var factory = {}
+      ref = new Firebase(FIREBASE_URL);
+
+      $routeScope.user = ref.getAuth();
+
+
+    factory.login = function(email, pass, cb) {
+      ref.authWithPassword({
+        email: email,
+        password: pass
+      }, function (error, authData) {
+        if (error === null) {
+          console.log("successful login", authData);
+          $rootScope.user = authData;
+          ref.child('users').child(authData.uid).child('authData').set(authData);
+          cb();
+        } else {
+          console.log("error logging in", error);
+        }
+      }
+      );
+    }
+    
+    })
+
+
+
+    .controller('LoginController', function(authFactory, $scope, $location){
+      var vm = this;
+
+      vm.login = function(){
+        authFactory.login(vm.email, vm.password, function(){
+          $location.path('/findshops')
+          $scope.$apply();
+        });
+      }
+      
+    })
+
+
+
 
   var ref = new Firebase('https://buzzhub.firebaseio.com/');
   ref.createUser({
