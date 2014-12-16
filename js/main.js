4b05c8071d17eb680f6b5542ab470d49eef76ae1
Spencer Sharpe <jspencersharpe@gmail.com>
2014-12-16 15:42:29 -0600

    slight refactoring

--- js/main.js
+++ js/main.js
@@ -1,53 +1,18 @@
-var lat, lng;
+;(function(){
+  'use strict';
 
-if (window.navigator.geolocation) {
-    var failure, success;
-        success = function(position) {
-           lat = position.coords.latitude;
-           lng = position.coords.longitude;
-      console.log(position.coords.latitude, position.coords.longitude);
-      var fsq = "https://api.foursquare.com/v2/venues/explore?ll="+lat+","+lng+"&client_id=GNPQME4WJXXAHQIRSIMCVLOOLS05DXJWYJZD3GESHMQX2NVO&client_secret=VKZOUFXSD4M5CF3UH1E4M0RL2YQL111HTCZH1DD0S5T5FQ3I&v=20130619&query="+$("#query").val()+""; 
-      getData(fsq);
-      };
-    failure = function(message) {
-      alert('Cannot retrieve location!');
-    };
-    navigator.geolocation.getCurrentPosition(success, failure, {
-      maximumAge: Infinity,
-      timeout: 5000
-     });     
-    }
+  angular.module('buzzHub', [])
 
-function getData(fsq){
-$.getJSON(fsq, function( data ) {
-    console.log(data)
-    data = (data.response.groups[0].items)
-    var items = [];
-    $.each( data, function( key) {
-    var stores = (data[key].venue.name)
-    items.push( "<li id='" + "'>" + stores + "</li>" );
-  });
- 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
-  });
-}
+  var ref = new Firebase('https://buzzhub.firebaseio.com/');
+  ref.createUser({
+  email    : "bobtony@firebase.com",
+  password : "correcthorsebatterystaple"
+}, function(error) {
+  if (error === null) {
+    console.log("User created successfully");
+  } else {
+    console.log("Error creating user:", error);
+  }
+});
 
-
-
-
-
-
-
- 
-
-   
-
-
-
-  
-
-
-
+}());
