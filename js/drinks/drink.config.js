791a75f3ea8909fd25f638be102c4162da553e4e
Spencer Sharpe <jspencersharpe@gmail.com>
2014-12-18 15:08:39 -0600

    controller, config, factory

---
+++ js/drinks/drink.config.js
@@ -0,0 +1,32 @@
+;(function(){
+  'use strict';
+
+  angular.module('buzzHub')
+    .config(function($routeProvider){
+      $routeProvider
+      .when('/drinks', {
+        templateUrl: 'views/table.html',
+        controller: 'DrinkController',
+        controllerAs: 'drink',
+        private: true
+      })
+      .when('/drinks/new' {
+        templateUrl: 'views/form.html',
+        controller: 'CoffeeController',
+        controllerAs: 'coffee',
+        private: true
+      })
+      .when('/drinks/:id', {
+        templateUrl: 'views/show.html',
+        controller: 'ShowController',
+        controllerAs: 'show',
+        private: true;
+      })
+      .when('/drinks/:id/edit', {
+        templateUrl: 'views/form.html',
+        controller: 'EditController',
+        controllerAs: 'coffee',
+        private: true
+      })
+    })
+}());
