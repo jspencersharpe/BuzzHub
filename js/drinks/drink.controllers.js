7bf382a7ff3e1503f66e9e53a951f2cf5d686490
Spencer Sharpe <jspencersharpe@gmail.com>
2014-12-18 15:49:29 -0600

    working configs, controllers, and factories

---
+++ js/drinks/drink.controllers.js
@@ -0,0 +1,60 @@
+;(function(){
+  'use strict';
+
+  angular.module('buzzHub')
+    .controller('ShowController', function($routeParams, todoFactory){
+      var vm = this;
+      var id = $routeParams.id;
+      coffeeFactory.getCoffee(id, function(data){
+        vm.task = data;
+      });
+    })
+    .controller('EditController', function($routeParams, coffeeFactory){
+      var vm = this;
+      var id = $routeParams.id;
+
+      coffeeFactory.getCoffee(id, function(data){
+        vm.newDrink = data;
+      });
+
+      vm.addNewDrink = function(){
+        coffeeFactory.editCoffee(id, vm.newDrink);
+      };
+
+      vm.menuOptions = coffeeFactory.menuOptions;
+
+    })
+    .controller('CoffeeController', function(coffeeFactory){
+       var vm = this;
+
+      coffeeFactory.getAllDrinks(function(data){
+        vm.drinks = data;
+      });
+
+      vm.addNewDrink = function(){
+        coffeeFactory.createCoffee(vm.newDrink, function(data){
+          vm.drinks = vm.drinks || {};
+          vm.drinks[data.name] = vm.newDrink;
+          vm.newDrink = _freshDrink();
+        });
+      };
+
+      vm.removeCoffee = function(coffeeId) {
+        coffeeFactory.deleteCoffee(coffeeId, function(){
+          delete vm.drinks[coffeeId];
+        });
+      };
+
+      vm.newDrink = _freshDrink();
+
+      vm.menuOptions = coffeeFactory.menuOptions;
+
+      function _freshDrink(){
+        return {
+          menu: 'coffee'
+        };
+      }
+
+    });
+    
+}());
