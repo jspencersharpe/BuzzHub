6409e9d1025c63c16743fb1a757289c42ac97679
jspencersharpe <jspencersharpe@gmail.com>
2015-03-26 14:39:21 -0500

    images posting, and code refactored

--- 2BuzzHub/Scripts/app/Controllers/coffeeController.js
+++ 2BuzzHub/Scripts/app/Controllers/coffeeController.js
@@ -1,85 +1,113 @@
-﻿'use strict';
-angular.module('buzzHub')
-app.controller('coffeeController', ['dataFactory', 'localStorageService', 'Coffee', '$scope', '$http', '$location', '$rootScope', 'uploadPhotoFactory', '$upload',
-                            function (dataFactory, localStorageService, Coffee, $scope, $http, $location, $rootScope, uploadPhotoFactory, $upload) {
+﻿;(function(){
 
-    $scope.coffee = {};
-    var vm = this;
+    'use strict';
+    angular.module('buzzHub')
+    app.controller('coffeeController', ['dataFactory', 'localStorageService', 'Coffee', '$scope', '$http', '$location', '$rootScope', 'uploadPhotoFactory', '$upload',
+                                function (dataFactory, localStorageService, Coffee, $scope, $http, $location, $rootScope, uploadPhotoFactory, $upload) {
 
-    //Foursquare API
-    dataFactory.getData(function (items) {
-        $scope.items = _.uniq(items);
-        $scope.$digest();
-    });
+                                    $scope.coffee = {};
+                                    var vm = this;
 
-    //GET
-    $scope.drinks = Coffee.get();
-    console.log($scope.drinks);
+                                    //Foursquare API
+                                    dataFactory.getData(function (items) {
+                                        $scope.items = _.uniq(items);
+                                        $scope.$digest();
+                                    });
+
+                                    //GET
+                                    $scope.drinks = Coffee.get();
+                                    console.log($scope.drinks);
                                 
-    //POST
-    $scope.postCoffee = function () {
+                                    //POST
+                                    $scope.postCoffee = function () {
 
-        $http.post('api/Coffee', $scope.coffee)
-        .catch(function (error) {
-            console.log(error);
-        })
-    };
+                                        $http.post('api/Coffee', $scope.coffee)
+                                        .catch(function (error) {
+                                            console.log(error);
+                                        })
+                                    };
 
-    $scope.deleteCoffee = function (id) {
+                                    $scope.deleteCoffee = function (id) {
         
-        Coffee.deleteCoffee({id: id});
-    }
+                                        Coffee.deleteCoffee({id: id});
+                                    }
 
-    vm.uploadFile = function () {
-        console.log('up')
-        upload.uploadPhoto(file, $rootScope, coffeeId);
-    };
+                                    vm.fileSelected = function (files) {
+                                        _setThumbnail(files[0], function (base64) {
+                                            $scope.$parent.coffee.files[0].dataUrl = base64;
+                                            $scope.$apply();
+                                        });
+                                    };
 
-    vm.uploadPhoto = function (cb) {
-        alert($scope.coffee.imagePath[0].name);
-        var file = $scope.coffee.imagePath[0];
-        $upload.upload({
-            url: 'https://buzzhub.s3.amazonaws.com',
-            method: 'POST',
-            data: {
-                'Content-Type': file.type,
-                key: 'dotnetimages/' + file.name,
-                acl: 'public-read',
-                awsaccesskeyid: 'AKIAJZO3MXQYNUR5MGJQ',
-                policy: 'eyJleHBpcmF0aW9uIjoiMjAyMC0wMS0wMVQwMDowMDowMFoiLCJjb25kaXRpb25zIjpbeyJidWNrZXQiOiJidXp6aHViIn0seyJhY2wiOiAicHVibGljLXJlYWQifSxbInN0YXJ0cy13aXRoIiwiJENvbnRlbnQtVHlwZSIsIiJdLFsic3RhcnRzLXdpdGgiLCIka2V5IiwiIl1dfQ==',
-                signature: 'pT0seLTIFqiwuawmR++yCZbN/Q4=',
-                file: file
-            },
-            fields: {
-                key: 'dotnetimages/' + file.name
-            }
-            })
-               .success(function (data, status, headers, config) {
-                   var filelink = 'https://buzzhub.s3.amazonaws/dotnetimages' + '/' + config.file.name;
-                   console.log('success');
-                   cb(filelink)
+                                    vm.uploadFile = function () {
+                                        console.log('up')
+                                        var file = $scope.coffee.imagePath[0];
+                                        uploadPhotoFactory.uploadPhoto(file, $rootScope);
+                                    };
+
+                                    function _setThumbnail(image, cb) {
+                                        _imageToBase64(image, function (data) {
+                                            cb(data)
+                                        });
+                                    }
+
+                                    function _imageToBase64(file, cb) {
+                                        if (file && file.type.indexOf('image') > -1) {
+                                            var fr = new FileReader();
+                                            fr.readAsDataURL(file);
+                                            fr.onload = function (e) {
+                                                cb(e.target.result);
+                                            }
+                                        }
+                                    }
+
+                                    $('#uploadButton').on('click', function (e) {
+                                        e.preventDefault();
+                                        e.stopPropagation();
+                                        vm.uploadFile();
+                                    });
+                                }])
+
+       .factory('uploadPhotoFactory', ['$upload', function ($upload) {
+           var factory = {}
+
+           factory.uploadPhoto = function (file, coffeeId, fileName, cb) {
+               // var file = filesArray[0];
+               $upload.upload({
+                   url: 'https://buzzhub.s3.amazonaws.com',
+                   method: 'POST',
+                   data: {
+                       'Content-Type': file.type,
+                       key: 'dotnetimages/' + file.name,
+                       acl: 'public-read',
+                       awsaccesskeyid: 'AKIAJZO3MXQYNUR5MGJQ',
+                       policy: 'eyJleHBpcmF0aW9uIjoiMjAyMC0wMS0wMVQwMDowMDowMFoiLCJjb25kaXRpb25zIjpbeyJidWNrZXQiOiJidXp6aHViIn0seyJhY2wiOiAicHVibGljLXJlYWQifSxbInN0YXJ0cy13aXRoIiwiJENvbnRlbnQtVHlwZSIsIiJdLFsic3RhcnRzLXdpdGgiLCIka2V5IiwiIl1dfQ==',
+                       signature: 'pT0seLTIFqiwuawmR++yCZbN/Q4=',
+                       file: file
+                   },
+                   fields: {
+                       key: 'dotnetimages/' + file.name
+                   }
                })
-               .error(function (err) {
-                   console.log('s3 problem', err);
-               });
-        }
+                      .success(function (data, status, headers, config) {
+                          var filelink = 'https://s3.amazonaws.com/buzzhub/dotnetimages' + '/' + file.name;
+                          console.log('success');
+                          typeof cb === 'function' && cb();
+                      })
+                      .error(function (err) {
+                          console.log('s3 problem', err);
+                      });
+           };   
+           return factory;
 
-        vm.fileSelected = function (files) {
-            _setThumbnail(files[0], function (base64) {
-                $scope.$parent.coffee.files[0].dataUrl = base64;
-                $scope.$apply();
-            });
-        };
+       }]);
 
-        $('#uploadButton').on('click', function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-            vm.uploadPhoto();
-        });
-                                  
+})();
+
+
 
   
-}]);
+
 
 //; (function () {
 //    'use strict';
--- 2BuzzHub/Scripts/app/Controllers/uploadController.js
+++ 2BuzzHub/Scripts/app/Controllers/uploadController.js
@@ -1,29 +1,3 @@
 ﻿'use strict';
 angular.module('buzzHub')
-    .factory('uploadPhotoFactory', ['$upload', function ($upload) {
-        var factory = {}
-
-        factory.uploadPhoto = function (file, coffeeId, fileName, cb) {
-            var file = filesArray[0];
-           
-        }
-
-        function _setThumbnail(image, cb) {
-            _imageToBase64(image, function (data) {
-                cb(data)
-            });
-        }
-
-        function _imageToBase64(file, cb) {
-            if (file && file.type.indexOf('image') > -1) {
-                var fr = new FileReader();
-                fr.readAsDataURL(file);
-                fr.onload = function (e) {
-                    cb(e.target.result);
-                }
-            }
-        }
-        return factory;
-        
-
-    }])
+    
