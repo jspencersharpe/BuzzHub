a840ce443052963842a8b30d06ebe4b2ce145f43
jspencersharpe <jspencersharpe@gmail.com>
2015-03-27 09:29:45 -0500

    bug fixes

--- 2BuzzHub/obj/Release/Package/PackageTmp/Scripts/app/Controllers/coffeeController.js
+++ 2BuzzHub/obj/Release/Package/PackageTmp/Scripts/app/Controllers/coffeeController.js
@@ -1,4 +1,4 @@
-﻿;(function(){
+﻿; (function () {
 
     'use strict';
     angular.module('buzzHub')
@@ -16,11 +16,10 @@
         //GET
         $scope.drinks = Coffee.get();
         console.log($scope.drinks);
-                                
+
         //POST
 
         $scope.postCoffee = function () {
-            alert('fired');
             $http.post('api/Coffee', $scope.coffee)
             .catch(function (error) {
                 console.log(error);
@@ -28,8 +27,8 @@
         };
 
         $scope.deleteCoffee = function (id) {
-        
-            Coffee.deleteCoffee({id: id});
+
+            Coffee.deleteCoffee({ id: id });
         }
 
         vm.fileSelected = function (files) {
@@ -41,6 +40,7 @@
 
         vm.uploadFile = function () {
             console.log('up')
+            alert('image added!');
             var file = $scope.coffee.imagePath[0];
             uploadPhotoFactory.uploadPhoto(file, function (filelink) {
                 $scope.coffee.imagePath = filelink;
@@ -90,7 +90,7 @@
                    fields: {
                        key: 'dotnetimages/' + file.name
                    }
-                   })
+               })
                       .success(function (data, status, headers, config) {
                           var filelink = 'https://s3.amazonaws.com/buzzhub/dotnetimages' + '/' + file.name;
                           console.log('success');
@@ -99,7 +99,7 @@
                       .error(function (err) {
                           console.log('s3 problem', err);
                       });
-           };   
+           };
            return factory;
 
        }]);
@@ -108,77 +108,5 @@
 
 
 
-  
 
 
-//; (function () {
-//    'use strict';
-
-//    angular.module('buzzHub')
-//     .controller('ShowController', ['$routeParams', 'coffeeFactory', 'dataFactory', /*'uploadPhotoFactory',*/
-//                           function ($routeParams, coffeeFactory, dataFactory /*, uploadPhotoFactory */) {
-//                               var vm = this;
-//                               var id = $routeParams.id;
-//                               coffeeFactory.getCoffee(id, function (data) {
-//                                   vm.drink = data;
-//                                   vm.drink.id = id;
-//                               });
-//                           }])
-
-//    .controller('EditController', ['$routeParams', 'dataFactory', 'coffeeFactory', /*'uploadPhotoFactory',*/
-//                          function ($routeParams, dataFactory, coffeeFactory /*, uploadPhotoFactory */) {
-//                              var vm = this;
-//                              var id = $routeParams.id;
-
-//                              coffeeFactory.getCoffee(id, function (data) {
-//                                  vm.newDrink = data;
-//                              });
-
-//                              vm.addNewDrink = function () {
-//                                  coffeeFactory.editCoffee(id, vm.newDrink);
-//                              };
-
-//                              vm.menuOptions = coffeeFactory.menuOptions;
-
-//                          }])
-//      .controller('coffeeController', ['coffeeFactory', '$scope', '$rootScope', 'dataFactory', /*'uploadPhotoFactory',*/
-//                              function (coffeeFactory, $scope, $rootScope, dataFactory /*, uploadPhotoFactory*/) {
-//                                  var vm = this;
-
-//                                  dataFactory.getData(function (items) {
-//                                      vm.items = _.uniq(items);
-//                                      $scope.$digest();
-//                                  })
-
-//                                  coffeeFactory.getAllDrinks(function (data) {
-//                                      vm.drinks = data;
-//                                  });
-
-//                                  vm.addNewDrink = function () {
-//                                      coffeeFactory.createCoffee(vm.newDrink, function (data) {
-//                                          vm.drinks = vm.drinks || {};
-//                                          vm.drinks[data.name] = vm.newDrink;
-//                                          uploadPhotoFactory.uploadPhoto(vm.files[0], $rootScope.user.uid, data.name)
-//                                          vm.newDrink = _freshDrink();
-//                                      });
-//                                  };
-
-//                                  vm.removeCoffee = function (coffeeId) {
-//                                      coffeeFactory.deleteCoffee(coffeeId, function () {
-//                                          delete vm.drinks[coffeeId];
-//                                      });
-//                                  };
-
-//                                  vm.newDrink = _freshDrink();
-
-//                                  vm.menuOptions = coffeeFactory.menuOptions;
-
-//                                  function _freshDrink() {
-//                                      return {
-//                                          drink: 'coffee',
-//                                      };
-//                                  }
-
-//                              }]);
-
-//})();
