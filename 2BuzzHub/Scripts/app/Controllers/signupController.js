011e70f531d9f67a4f1f868e78e19e5e5350ecb7
jspencersharpe <jspencersharpe@gmail.com>
2015-04-08 10:03:12 -0500

    fixed SignUp

--- 2BuzzHub/Scripts/app/Controllers/signupController.js
+++ 2BuzzHub/Scripts/app/Controllers/signupController.js
@@ -1,25 +1,29 @@
 ﻿'use strict';
 angular.module('buzzHub')
 .controller('SignupController', ['$scope', '$location', 'authService',
-                        function ($scope, $location, authService) {
+    function ($scope, $location, authService) {
 
-                             $scope.signup = function () {
-                                 authService.register($scope.user).then(
-                                     function (res) {
-                                         console.log(res);
-                                     },
-                                     function (err) {
-                                         console.log(err);
-                                         $scope.errors = [];
-                                         ers = err.data.ModelState;
-                                         for (var key in ers) {
-                                             ers[key].forEach(function (el) {
-                                                 $scope.errors.push(el);
-                                             })
-                                         }
-                                     }
-                                 );
-                                 $scope.user = {};
-                             };
+        $scope.signup = function () {
+            var usr = $scope.user;
+            authService.register(usr).then(
+                    function (res) {
+                        authService.login({ username: usr.Email, password: usr.Password })
+                        .then(function () {
+                            $location.path('landing');
+                        }, function (err) {
+                            console.log(err);
+                        });
+                    },
+                    function (err) {
+                        err = err.data.ModelState;
+                        for (var key in err) {
+                            err[key].forEach(function (el) {
+                                $scope.errors.push(el);
+                            })
+                        }
+                    }
+                );
+                $scope.user = {};
+            };
 
-                         }])
+        }])
